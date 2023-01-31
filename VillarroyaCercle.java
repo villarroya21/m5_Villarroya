c995eb575f5123d98ebffeea7a8a97ba1b99ec11
villarroya21 <117456238+villarroya21@users.noreply.github.com>
2023-01-31 10:35:13 +0100

    codi verificat.

--- VillarroyaCercle.java
+++ VillarroyaCercle.java
@@ -2,7 +2,7 @@ public class VillarroyaCercle {
     public static void main(String[] args) {
       double pi = 3.14;
       double r = 7;
-      double s = pi * r * r;
+      //double s = pi * r * r;
       System.out.println("La superficie del cercle sera " + s);
     }
   }
