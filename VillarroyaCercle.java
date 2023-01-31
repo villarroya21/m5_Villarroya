f8b6e565a5f8caa8c86c1187ebfc7d9666fc13f0
villarroya21 <117456238+villarroya21@users.noreply.github.com>
2023-01-31 10:19:48 +0100

    codi

---
+++ VillarroyaCercle.java
@@ -0,0 +1,8 @@
+public class VillarroyaCercle {
+    public static void main(String[] args) {
+      double pi = 3.14;
+      double r = 7;
+      double s = pi * r * r;
+      System.out.println("La superficie del cercle sera " + s);
+    }
+  }
